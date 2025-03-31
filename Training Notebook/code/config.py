534048ac11457417d1bfb0fa32232ad375070b28
rohit49plus2 <muralirohit@gmail.com>
2025-03-31 14:12:17 -0700

    update config

--- Training Notebook/code/config.py
+++ Training Notebook/code/config.py
@@ -1,9 +1,9 @@
 model = "RF"
 # model = "XGB"
 
-training_s3_path = 's3://halton-ubc/model/' #change the path to the S3 bucket for model artifact output (same bucket that should trigger the endpoint creation lambda in deployment guide)
+training_s3_path = 's3://my-bucket/model/' #change the path to the S3 bucket for model artifact output (same bucket that should trigger the endpoint creation lambda in deployment guide)
 
-data_bucket = 'halton-ubc'#where your training data is stored
+data_bucket = 'my-bucket'#where your training data is stored
 training_path = 'data/'#folder inside data bucket for training data csv
 
 
